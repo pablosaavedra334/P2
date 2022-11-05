a3a30c41ce40581b9ba6bbae3837b48e563bc8e0
pablosaavedra334 <pablosaavedra334@gmail.com>
2022-11-04 20:19:03 -0400

    Carga inicial

---
+++ P2s/P2s/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2s.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
